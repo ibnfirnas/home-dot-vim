97bf630b3d3c64afdb235c2969b6eacb3e4a619c
Siraaj Khandkar <siraaj@khandkar.net>
2011-11-29 11:15:10 -0500

    Reorganized dierctory structure.

--- install.py
+++ install.py
@@ -6,6 +6,7 @@ import shutil
 
 
 PATH_DIR__THIS = os.path.abspath(os.path.dirname(__file__))
+PATH_DIR__SRC = os.path.join(PATH_DIR__THIS, 'src')
 PATH_DIR__HOME = os.path.expanduser('~')
 
 PROJECT_NODES = ['.vim', '.vimrc', '.gvimrc']
@@ -13,7 +14,7 @@ PROJECT_NODES = ['.vim', '.vimrc', '.gvimrc']
 
 def main():
     for node in PROJECT_NODES:
-        src_path = os.path.join(PATH_DIR__THIS, node)
+        src_path = os.path.join(PATH_DIR__SRC, node)
         dst_path = os.path.join(PATH_DIR__HOME, node)
 
         if os.path.exists(dst_path):
--- update.py
+++ update.py
@@ -5,13 +5,14 @@ import os
 
 
 PATH_DIR__THIS = os.path.abspath(os.path.dirname(__file__))
-PATH_DIR__REPOS = os.path.join(PATH_DIR__THIS, 'repos')
-PATH_DIR__VIM = os.path.join(PATH_DIR__THIS, '.vim')
+PATH_DIR__DEPS = os.path.join(PATH_DIR__THIS, 'deps')
+PATH_DIR__SRC = os.path.join(PATH_DIR__THIS, 'src')
+PATH_DIR__VIM = os.path.join(PATH_DIR__SRC, '.vim')
 
 
 def main():
-    for repo_name in os.listdir(PATH_DIR__REPOS):
-        repo_path = os.path.join(PATH_DIR__REPOS, repo_name)
+    for repo_name in os.listdir(PATH_DIR__DEPS):
+        repo_path = os.path.join(PATH_DIR__DEPS, repo_name)
 
         print 'Installing', repo_name, '...'
 
